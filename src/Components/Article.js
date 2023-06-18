d13d248bcdcb7b457c8a1f86a2551db043242ef4
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-06-18 17:59:26 +0530

    Articles

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,5 +1,18 @@
-export default function Article() {
+export default function Article({ title, description, image, category, date }) {
     return (
-        <div>Article</div>
+        <div className="article">
+            <div className="thumbnail">
+                <img src={image} alt="thumbnail" />
+            </div>
+            <div className="information">
+                <div className="meta">
+                    <div className="category">{category}</div>
+                    <div className="date">{date}</div>
+                </div>
+                <div className="title">{title}</div>
+                <div className="description">{description}</div>
+                <a className="read-more" href="#">Read more -&gt; </a>
+            </div>
+        </div>
     )
 }
--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,14 +1,74 @@
 import Article from './Article'
 import TagFilter from './TagFilter'
+import "../Styles/Articles.css"
+
 
 export default function Articles() {
     return (
         <div>
             <TagFilter />
-            <Article />
-            <Article />
-            <Article />
-            <Article />
+            <div className='articles'>
+                <div className='articles-row'>
+                    <Article
+                        image="/Thumbnails/Thumbnail-1.png"
+                        category="Lifestyle"
+                        date="2 hours ago"
+                        title="Changing people's lifestyles for the better"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-2.png"
+                        category="Technology"
+                        date="2 hours ago"
+                        title="Simple Solutions for Complex Connections"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-3.png"
+                        category="Sport"
+                        date="2 hours ago"
+                        title="All things are difficult before they are easy"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-4.png"
+                        category="Education"
+                        date="2 hours ago"
+                        title="How to Memorize everything you read?"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                </div>
+                <div className='articles-row'>
+                    <Article
+                        image="/Thumbnails/Thumbnail-1.png"
+                        category="Lifestyle"
+                        date="2 hours ago"
+                        title="Changing people's lifestyles for the better"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-2.png"
+                        category="Technology"
+                        date="2 hours ago"
+                        title="Simple Solutions for Complex Connections"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-3.png"
+                        category="Sport"
+                        date="2 hours ago"
+                        title="All things are difficult before they are easy"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                    <Article
+                        image="/Thumbnails/Thumbnail-4.png"
+                        category="Education"
+                        date="2 hours ago"
+                        title="How to Memorize everything you read?"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus"
+                    />
+                </div>
+            </div>
         </div>
     )
 }

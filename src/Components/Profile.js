5238f22b2bc066ab9560a3f447b52ddfcea317d4
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-06-18 16:53:43 +0530

    Navbar

---
+++ src/Components/Profile.js
@@ -0,0 +1,9 @@
+export default function Profile() {
+    return (
+        <div className="profile">
+            <div className="profile-img">
+                <img src="https://www.pngfind.com/pngs/m/676-6764065_default-profile-picture-transparent-hd-png-download.png" alt="" />
+            </div>
+        </div>
+    )
+}
